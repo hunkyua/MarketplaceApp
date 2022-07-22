516af2c01c8025830ea8461d5131e23169763b2d
VOpanasyuk <opanasyuk.valentin@gmail.com>
2022-07-22 10:03:02 +0300

    marketplace initialization

---
+++ src/test/java/com/hunky/marketplaceapp/MarketplaceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.hunky.marketplaceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
